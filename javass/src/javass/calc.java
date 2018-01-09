da1d687d45e65e266134b20a7f5ce657838ee6bc
JunYeong Kim <JunYeong Kim@jun>
2018-01-09 18:33:47 +0900

    initial calc made with menu print function

---
+++ javass/src/javass/calc.java
@@ -0,0 +1,55 @@
+package javass;
+import java.util.*;
+
+public class calc {
+
+public calc() {
+		
+	}
+	
+	
+	
+	
+	void print_menu() {
+		System.out.println("[ ID: 1772424 ]");
+		System.out.println("[ NAME: JunYeong Kim ]");
+		System.out.println("=======MENU=======");
+		System.out.println("1. Add two numbers");
+		System.out.println("2. Subtract two numbers");
+		System.out.println("3.Multiply two numbers");
+		System.out.println("4. Quit");			
+	}
+	
+	int getNum() {
+		int input=0;
+		System.out.print("> : ");	
+		Scanner s = new Scanner(System.in);
+		input = s.nextInt();
+		return input;
+	}
+	
+	public static void main(String [] args) {
+		calc a1 = new calc();
+		int result =0;
+		int num1=0;
+		int num2=0;
+		a1.print_menu();
+		int a = a1.getNum();
+		if (a == 4) {
+			System.out.println("system end");
+			System.exit(0);
+		}
+		
+		else if(a == 1 || a ==2 || a==3) {
+			num1 = a1.getNum();
+			num2 = a1.getNum();		
+			
+		}
+		else System.out.println("error");
+		
+		
+		
+		
+	}
+	
+}
